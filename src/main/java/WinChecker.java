0be671c7eb56ccc5b2b857204191a89d7997190b
Francesco Ciriaco <francescociriaco15@gmail.com>
2026-01-23 22:57:13 +0100

    refactoring riuscito e convalidato dai test

--- src/main/java/WinChecker.java
+++ src/main/java/WinChecker.java
@@ -6,7 +6,7 @@ public class WinChecker {
         this.grid = grid;
     }
 
-
+    /*
     private int consecutiveCount(int row, int column, int deltaRow, int deltaColumn) {
         int count = 0;
         Grid.Cross startCrossColor = grid.getCrossAt(row, column);
@@ -90,10 +90,45 @@ public class WinChecker {
         return count;
     }
 
-    private int consecutiveCountFromNextPosition(Grid.Position position, int deltaRow, int deltaColumn) {
+    */
+
+    private int consecutiveCountFromNextPosition(Grid.Position position, int deltaRow, int deltaColumn, Grid.Cross startCrossColor) {
         int nextRow = position.row() + deltaRow;
         int nextColumn = position.col() + deltaColumn;
-        return consecutiveCountOneDirections(new Grid.Position(nextRow, nextColumn), deltaRow, deltaColumn);
+        int count = 0;
+
+        while ( grid.contains(nextRow, nextColumn) && grid.getCrossAt(nextRow, nextColumn) == startCrossColor) {
+            count++;
+            nextRow += deltaRow;
+            nextColumn += deltaColumn;
+        }
+        return count;
+    }
+
+    private int consecutiveCountBothDirections(Grid.Position position, Direction direction, Grid.Cross startCrossColor) {
+        int deltaRow = direction.deltaRow();
+        int deltaColumn = direction.deltaColumn();
+
+        return 1 + consecutiveCountFromNextPosition(position, deltaRow, deltaColumn, startCrossColor)
+                 + consecutiveCountFromNextPosition(position, -deltaRow, -deltaColumn, startCrossColor);
+    }
+
+    public boolean isWinningMove(Grid.Position position) {
+        Grid.Cross startCrossColor = grid.getCrossAt(position);
+        if (startCrossColor == Grid.Cross.EMPTY) {
+            return false;
+        } //se la croce di partenza è vuota non può essere una mossa vincente
+
+        for (Direction direction : Direction.values()) {
+            if (consecutiveCountBothDirections(position, direction, startCrossColor) >= 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isWinningMove(int row, int column) {
+        return isWinningMove(new Grid.Position(row, column));
     }
 
 }
